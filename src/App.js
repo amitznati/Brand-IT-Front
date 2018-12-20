976c06c89e4652e42448d7b11ad580fe18784549
AmitZnati <amitznati@gmail.com>
2018-12-20 10:39:15 +0200

    Eslint init

--- src/App.js
+++ src/App.js
@@ -14,34 +14,34 @@ import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 if(localStorage.jwtToken) {
-  setAuthToken(localStorage.jwtToken);
-  const decoded = jwt_decode(localStorage.jwtToken);
-  store.dispatch(setCurrentUser(decoded));
+	setAuthToken(localStorage.jwtToken);
+	const decoded = jwt_decode(localStorage.jwtToken);
+	store.dispatch(setCurrentUser(decoded));
 
-  const currentTime = Date.now() / 1000;
-  if(decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-    window.location.href = '/login'
-  }
+	const currentTime = Date.now() / 1000;
+	if(decoded.exp < currentTime) {
+		store.dispatch(logoutUser());
+		window.location.href = '/login';
+	}
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store = { store }>
-        <Router>
-            <div>
-              <Navbar />
-                <Route exact path="/" component={ Home } />
-                <div className="container">
-                  <Route exact path="/register" component={ Register } />
-                  <Route exact path="/login" component={ Login } />
-                </div>
-            </div>
-          </Router>
-        </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider store = { store }>
+				<Router>
+					<div>
+						<Navbar />
+						<Route exact path="/" component={ Home } />
+						<div className="container">
+							<Route exact path="/register" component={ Register } />
+							<Route exact path="/login" component={ Login } />
+						</div>
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
